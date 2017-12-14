c63a41e2d594637a5f5da3914e1731583c8b3b46
Swati Rout <swatirout@prokarma-mac-mini.local>
2017-12-14 12:33:11 +0530

    commit view controller second time

--- gitex/ViewController.swift
+++ gitex/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("sw")
+        print("sw vfcg")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
